f48911d6862730ce3289f4642e49141a819a7595
perianangit <periananpdkt@gmail.com>
2022-04-27 09:55:47 +0530

    first commit

---
+++ src/main/java/com/app/admisssion/CollegeAdmissionAPI/CollegeAdmissionApiApplication.java
@@ -0,0 +1,13 @@
+package com.app.admisssion.CollegeAdmissionAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeAdmissionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeAdmissionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/admisssion/CollegeAdmissionAPI/CollegeAdmissionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.admisssion.CollegeAdmissionAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeAdmissionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
